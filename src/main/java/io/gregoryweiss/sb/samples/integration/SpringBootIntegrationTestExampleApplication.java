dd21f81943666fd962bbb47d768f9c0b11e60b19
gweiss27 <gweiss27@gmail.com>
2017-05-10 17:48:32 -0700

    First Commit

---
+++ src/main/java/io/gregoryweiss/sb/samples/integration/SpringBootIntegrationTestExampleApplication.java
@@ -0,0 +1,12 @@
+package io.gregoryweiss.sb.samples.integration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootIntegrationTestExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootIntegrationTestExampleApplication.class, args);
+	}
+}
---
+++ src/test/java/io/gregoryweiss/sb/samples/integration/SpringBootIntegrationTestExampleApplicationTests.java
@@ -0,0 +1,16 @@
+package io.gregoryweiss.sb.samples.integration;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootIntegrationTestExampleApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
